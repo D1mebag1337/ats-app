76fd514b400e028f900ab05262768381288dfe62
D1mebag1337 <czychinico97@hotmail.com>
2026-03-22 13:44:13 +0100

    Haelfte vergessen

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
+use Inertia\Inertia;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
